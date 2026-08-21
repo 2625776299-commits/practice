3b6146b6e78dd2726e9ae587160dea3cb4adf366
hyseek <2625776299@qq.com>
2026-08-21 19:21:26 +0800

    mew content

--- api_tools.py
+++ api_tools.py
@@ -18,3 +18,22 @@ def get_name_address(data):
         address=user['address']['city']
         name_address.append([name,address])
     return name_address
+
+def search_users(url, params):
+    try:
+        response = requests.get(url, params=params, timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("请求失败：", e)
+        return None
+
+
+def create_post(url, post_data):
+    try:
+        response = requests.post(url, json=post_data, timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("请求失败：", e)
+        return None
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 
 import api_tools
-url = "https://jsonplaceholder.typicode.com/users"
-
-data = api_tools.get_url(url)
 
 
+url_1 = "https://jsonplaceholder.typicode.com/users"
+url_2 = "https://jsonplaceholder.typicode.com/posts"
+params={
+    "username": "Bret",
 
 
-print("所有用户姓名：")
-print(api_tools.get_names(data))
-print(api_tools.get_name_address(data))
+}
+post_data = {
+    "title": "AI Learning",
+    "body": "Day 8.21 API practice",
+    "userId": 1
+}
+data = api_tools.get_url(url_1)
+
+get_=api_tools.search_users(url_1, params)
+post_=api_tools.create_post(url_2, post_data)
 
 
+# print("所有用户姓名：")
+# print(api_tools.get_names(data))
+# print(api_tools.get_name_address(data))
+
+print(get_)
+print(post_)
 
