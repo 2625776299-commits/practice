f6d2d6dc87f6e4a62bd406f19610b751349d1981
hyseek <2625776299@qq.com>
2026-08-21 19:37:14 +0800

    new content

--- main.py
+++ main.py
@@ -14,15 +14,11 @@ post_data = {
     "body": "Day 8.21 API practice",
     "userId": 1
 }
-data = api_tools.get_url(url_1)
+
 
 get_=api_tools.search_users(url_1, params)
 post_=api_tools.create_post(url_2, post_data)
 
-
-# print("所有用户姓名：")
-# print(api_tools.get_names(data))
-# print(api_tools.get_name_address(data))
 
 print(get_)
 print(post_)
